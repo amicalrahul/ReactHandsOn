af4e32b8577cb517b60c0800857fd1a133e9523f
Rahul Sabharwal <amicalrahul@gmail.com>
2018-03-13 23:02:34 +1100

    Display props using js dataobject defined with js file

--- ReactHandsOn/Scripts/Tutorial.jsx
+++ ReactHandsOn/Scripts/Tutorial.jsx
@@ -1,9 +1,15 @@
-﻿var CommentBox = React.createClass({
+﻿var data = [
+    { Id: 1, Author: "Daniel Lo Nigro", Text: "Hello ReactJS.NET World!" },
+    { Id: 2, Author: "Pete Hunt", Text: "This is one comment" },
+    { Id: 3, Author: "Jordan Walke", Text: "This is *another* comment" }
+];
+
+var CommentBox = React.createClass({
     render: function () {
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList />
+                <CommentList data={this.props.data} />
                 <CommentForm />
       </div>
         );
@@ -12,11 +18,16 @@
 
 var CommentList = React.createClass({
     render: function () {
+        var commentNodes = this.props.data.map(function (comment) {
+            return (
+                <Comment author={comment.Author} key={comment.Id}>
+                    {comment.Text}
+                </Comment>
+                )
+        });
         return (
             <div className="commentList">
-                <Comment author="Daniel Lo Nigro">Hello ReactJS.NET World!</Comment>
-                <Comment author="Pete Hunt">This is one comment</Comment>
-                <Comment author="Jordan Walke">This is *another* comment</Comment>
+                {commentNodes}
             </div>
         );
     }
@@ -30,7 +41,8 @@ var CommentForm = React.createClass({
       </div>
         );
     }
-}); var Comment = React.createClass({
+});
+var Comment = React.createClass({
     rawMarkup: function () {
         var md = new Remarkable();
         var rawMarkup = md.render(this.props.children.toString());
@@ -49,6 +61,6 @@ var CommentForm = React.createClass({
     }
 });
 ReactDOM.render(
-    <CommentBox />,
+    <CommentBox data={data} />,
     document.getElementById('content')
 );
